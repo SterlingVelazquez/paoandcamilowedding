dc31a2ac08ba7eda4592d5992cb4cab5f4d58fd6
Sterling Velazquez <SterlingVelazquez@users.noreply.github.com>
2021-03-13 22:15:55 -0500

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,178 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 export default function Home() {
+
+  function FadeInSection(props) {
+    const [isVisible, setVisible] = React.useState(false);
+    const domRef = React.useRef();
+    React.useEffect(() => {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => setVisible(entry.isIntersecting));
+      });
+      observer.observe(domRef.current);
+    }, []);
+    return (
+      <div
+        className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+        ref={domRef}
+      >
+        {props.children}
+      </div>
+    );
+  }
+
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Pao and Camilo's Wedding</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Alice&family=Mulish:wght@200;300&display=swap" rel="stylesheet"></link>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main className="main-wrapper">
+        <div className="introDiv">
+          <img className="introImage" src="images/paocamiloengaged.jpg"></img>
+          <div className="introOverlay"></div>
+          <div className="introHeaderSection">
+            <p className="introTitle">Paola &amp; Camilo</p>
+            <div className="introDivider">SAVE THE DATE</div>
+            <p className="introDate">November 27, 2021</p>
+            <div className="centerCalendarBtn">
+              <button className="calendarBtn">ADD TO MY CALENDAR</button>
+            </div>
+          </div>
+        </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <div className="eventDiv">
+          <div className="eventHeaderSection">
+            <p className="eventHeader">Event Attractions</p>
+            <p className="eventSubHeader">Below you'll find everything you need to know to be ready for all the events.</p>
+            <img className="eventHeaderImage" src="images/rose-icon-01.png"></img>
+          </div>
+          <div className="eventGrid">
+            <div className="event">
+              <div className="iconBox">
+                <div className="iconHolder">
+                  <img className="eventIcon" src="briefcase.svg"></img>
+                </div>
+              </div>
+              <p className="eventTitle">Accomodation</p>
+              <p className="eventDescription">This is a short description elaborating the service you have mentioned above.</p>
+              <div className="smallBorder"></div>
+            </div>
+            <div className="event">
+              <div className="iconBox">
+                <div className="iconHolder">
+                  <img className="eventIcon" src="dress.svg"></img>
+                </div>
+              </div>
+              <p className="eventTitle">What To Wear</p>
+              <p className="eventDescription">This is a short description elaborating the service you have mentioned above.</p>
+              <div className="smallBorder"></div>
+            </div>
+            <div className="event">
+              <div className="iconBox">
+                <div className="iconHolder">
+                  <img className="eventIcon" src="car.svg"></img>
+                </div>
+              </div>
+              <p className="eventTitle">Parking Area</p>
+              <p className="eventDescription">This is a short description elaborating the service you have mentioned above.</p>
+              <div className="smallBorder"></div>
+            </div>
+            <div className="event">
+              <div className="iconBox">
+                <div className="iconHolder">
+                  <img className="eventIcon" src="camera.svg"></img>
+                </div>
+              </div>
+              <p className="eventTitle">Documentation</p>
+              <p className="eventDescription">This is a short description elaborating the service you have mentioned above.</p>
+              <div className="smallBorder"></div>
+            </div>
+            <div className="event">
+              <div className="iconBox">
+                <div className="iconHolder">
+                  <img className="eventIcon" src="party-hat.svg"></img>
+                </div>
+              </div>
+              <p className="eventTitle">Celebration</p>
+              <p className="eventDescription">This is a short description elaborating the service you have mentioned above.</p>
+              <div className="smallBorder"></div>
+            </div>
+            <div className="event">
+              <div className="iconBox">
+                <div className="iconHolder">
+                  <img className="eventIcon" src="food.svg"></img>
+                </div>
+              </div>
+              <p className="eventTitle">Food &amp; Drinks</p>
+              <p className="eventDescription">This is a short description elaborating the service you have mentioned above.</p>
+              <div className="smallBorder"></div>
+            </div>
+          </div>
+          <div className="slideshowDiv">
+            <div className="convolutedDiv">
+              <div className="slideshowSide"></div>
+              <div className="slideshowSide"></div>
+            </div>
+            <div className="slideshowImages">
+              <img className="slideshowImage" src="images/chicago.jpg"></img>
+              <img className="slideshowImage" src="images/column.jpg"></img>
+              <img className="slideshowImage" src="images/disney.jpg"></img>
+              <img className="slideshowImage" src="images/london.jpg"></img>
+              <img className="slideshowImage" src="images/mountain.jpg"></img>
+              <img className="slideshowImage" src="images/puertorico.jpg"></img>
+              <img className="slideshowImage" src="images/ski.jpg"></img>
+              <img className="slideshowImage" src="images/stadium.jpg"></img>
+            </div>
+          </div>
+        </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="locationDiv">
+          <div className="eventHeaderSection">
+            <p className="eventHeader">When &amp; Where</p>
+            <img className="eventHeaderImage" src="images/rose-icon-01.png"></img>
+          </div>
+          <div className="eventGrid">
+            <div className="locationBox">
+              <div className="firstIconBox">
+                <div className="firstIconHolder">
+                  <img className="firstEventIcon" src="heart.svg"></img>
+                </div>
+              </div>
+              <p className="firstLocationTitle">The Ceremony</p>
+              <p className="firstLocationDescription">Saturday, November 27, 2021<br/>
+                11:30 AM - 01.00 PM<br/>678 Central Square<br/>Cali, Colombia</p>
+            </div>
+            <div className="locationBox">
+              <div className="otherIconBox">
+                <div className="otherIconHolder">
+                  <img className="otherEventIcon" src="bouquet.svg"></img>
+                </div>
+              </div>
+              <p className="locationTitle">The Reception</p>
+              <p className="locationDescription">Saturday, November 27, 2021<br/>
+                11:30 AM - 01.00 PM<br/>678 Central Square<br/>Cali, Colombia</p>
+            </div>
+            <div className="locationBox">
+              <div className="otherIconBox">
+                <div className="otherIconHolder">
+                  <img className="otherEventIcon" src="party.svg"></img>
+                </div>
+              </div>
+              <p className="locationTitle">The Party</p>
+              <p className="locationDescription">Saturday, November 27, 2021<br/>
+                11:30 AM - 01.00 PM<br/>678 Central Square<br/>Cali, Colombia</p>
+            </div>
+          </div>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    
     </div>
   )
 }
+
