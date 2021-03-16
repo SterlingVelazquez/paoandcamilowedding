464fe6b101058826fd243ebc23593ad85568149f
Sterling Velazquez <SterlingVelazquez@users.noreply.github.com>
2021-03-16 15:29:03 -0400

    Added animations, specifically fade in on scroll using interaction observer

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import Head from 'next/head'
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export default function Home() {
 
-  function FadeInSection(props) {
+  function Fade(props) {
     const [isVisible, setVisible] = React.useState(false);
     const domRef = React.useRef();
     React.useEffect(() => {
@@ -15,7 +14,8 @@ export default function Home() {
     }, []);
     return (
       <div
-        className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+        className={`fade-in-section ${isVisible || (typeof domRef.current !== "undefined" &&
+          domRef.current.className.includes("is-visible")) ? 'is-visible' : ''}`}
         ref={domRef}
       >
         {props.children}
@@ -37,85 +37,85 @@ export default function Home() {
           <img className="introImage" src="images/paocamiloengaged.jpg"></img>
           <div className="introOverlay"></div>
           <div className="introHeaderSection">
-            <p className="introTitle">Paola &amp; Camilo</p>
-            <div className="introDivider">SAVE THE DATE</div>
-            <p className="introDate">November 27, 2021</p>
-            <div className="centerCalendarBtn">
-              <button className="calendarBtn">ADD TO MY CALENDAR</button>
-            </div>
+              <p className="introTitle">Paola &amp; Camilo</p>
+              <div className="introDivider">SAVE THE DATE</div>
+              <p className="introDate">November 27, 2021</p>
+              <div className="centerCalendarBtn">
+                <button className="calendarBtn">ADD TO MY CALENDAR</button>
+              </div>
           </div>
           <div className="introFade"></div>
         </div>
 
         <div className="eventDiv">
-          <div className="eventHeaderSection">
+          <Fade><div className="eventHeaderSection">
             <p className="eventHeader">Event Attractions</p>
             <p className="eventSubHeader">Below you'll find everything you need to know to be ready for all the events.</p>
             <img className="eventHeaderImage" src="images/rose-icon-01.png"></img>
-          </div>
+          </div></Fade>
           <div className="eventGrid">
             <div className="event">
-              <div className="iconBox">
+              <Fade><div className="iconBox">
                 <div className="iconHolder">
                   <img className="eventIcon" src="briefcase.svg"></img>
                 </div>
               </div>
               <p className="eventTitle">Accomodation</p>
               <p className="eventDescription">This is a short description elaborating the service you have mentioned above.</p>
-              <div className="smallBorder"></div>
+              <div className="smallBorder"></div></Fade>
             </div>
             <div className="event">
-              <div className="iconBox">
+              <Fade><div className="iconBox">
                 <div className="iconHolder">
                   <img className="eventIcon" src="dress.svg"></img>
                 </div>
               </div>
               <p className="eventTitle">What To Wear</p>
               <p className="eventDescription">This is a short description elaborating the service you have mentioned above.</p>
-              <div className="smallBorder"></div>
+              <div className="smallBorder"></div></Fade>
             </div>
             <div className="event">
-              <div className="iconBox">
+              <Fade><div className="iconBox">
                 <div className="iconHolder">
                   <img className="eventIcon" src="car.svg"></img>
                 </div>
               </div>
               <p className="eventTitle">Parking Area</p>
               <p className="eventDescription">This is a short description elaborating the service you have mentioned above.</p>
-              <div className="smallBorder"></div>
+              <div className="smallBorder"></div></Fade>
             </div>
             <div className="event">
-              <div className="iconBox">
+              <Fade><div className="iconBox">
                 <div className="iconHolder">
                   <img className="eventIcon" src="camera.svg"></img>
                 </div>
               </div>
               <p className="eventTitle">Documentation</p>
               <p className="eventDescription">This is a short description elaborating the service you have mentioned above.</p>
-              <div className="smallBorder"></div>
+              <div className="smallBorder"></div></Fade>
             </div>
             <div className="event">
-              <div className="iconBox">
+              <Fade><div className="iconBox">
                 <div className="iconHolder">
                   <img className="eventIcon" src="party-hat.svg"></img>
                 </div>
               </div>
               <p className="eventTitle">Celebration</p>
               <p className="eventDescription">This is a short description elaborating the service you have mentioned above.</p>
-              <div className="smallBorder"></div>
+              <div className="smallBorder"></div></Fade>
             </div>
             <div className="event">
-              <div className="iconBox">
+              <Fade><div className="iconBox">
                 <div className="iconHolder">
                   <img className="eventIcon" src="food.svg"></img>
                 </div>
               </div>
               <p className="eventTitle">Food &amp; Drinks</p>
               <p className="eventDescription">This is a short description elaborating the service you have mentioned above.</p>
-              <div className="smallBorder"></div>
+              <div className="smallBorder"></div></Fade>
             </div>
           </div>
-          <div className="slideshowDiv">
+          <Fade><div className="slideshowDiv">
             <div className="convolutedDiv">
               <div className="slideshowSide"></div>
               <div className="slideshowSide"></div>
@@ -130,17 +130,17 @@ export default function Home() {
               <img className="slideshowImage" src="images/ski.jpg"></img>
               <img className="slideshowImage" src="images/stadium.jpg"></img>
             </div>
-          </div>
+          </div></Fade>
         </div>
 
         <div className="locationDiv">
           <div className="locationFade"></div>
           <img className="locationImage" src="images/champagne.png"></img>
           <div className="eventHeaderSection">
-            <p className="eventHeader">When &amp; Where</p>
-            <img className="eventHeaderImage" src="images/rose-icon-01.png"></img>
+            <Fade><p className="eventHeader">When &amp; Where</p>
+            <img className="eventHeaderImage" src="images/rose-icon-01.png"></img></Fade>
           </div>
-          <div className="eventGrid">
+          <Fade><div className="eventGrid">
             <div className="locationBox">
               <div className="firstIconBox">
                 <div className="firstIconHolder">
@@ -173,7 +173,7 @@ export default function Home() {
             </div>
           </div>
           <p className="googlemapsp"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127450.10983778453!2d-76.59587550763219!3d3.3950618568448467!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e30a6f0cc4bb3f1%3A0x1f0fb5e952ae6168!2sCali%2C%20Valle%20del%20Cauca%2C%20Colombia!5e0!3m2!1sen!2sus!4v1615692973940!5m2!1sen!2sus" 
-            className="googlemap" allowfullscreen="" loading="lazy"></iframe></p>
+            className="googlemap" allowFullScreen="" loading="lazy"></iframe></p></Fade>
         </div>
       </main>
     
